6ab0228f2fb8c5d24ac9d8cc242a73efa3ccb569
lsh9672 <lsh80165@gmail.com>
2025-04-27 22:11:05 +0900

    Feat : 스웨거 추가(노드)

--- src/main/java/com/monitoring/backend/dto/req/node/NodeListReq.java
+++ src/main/java/com/monitoring/backend/dto/req/node/NodeListReq.java
@@ -1,5 +1,6 @@
 package com.monitoring.backend.dto.req.node;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,12 +10,22 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "노드 목록 조회 요청")
 public class NodeListReq {
 
 	//클러스터 식별자에 해당하는 노드 조회용
+	@Schema(
+		description = "클러스터 식별자(시퀀스)",
+		example = "1"
+	)
 	private long clusterSeq;
 
 	//마지막으로 조회한 seq
+	@Schema(
+		description = "마지막으로 호출된 노드 seq - 무한스크롤 용",
+		example = "1",
+		nullable = true
+	)
 	private long lastSeq;
 
 }
