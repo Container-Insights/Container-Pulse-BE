3b523dd0fd7c973d2c0b8589ee449b51b1bcb2a9
lsh9672 <lsh80165@gmail.com>
2025-04-27 22:10:54 +0900

    Feat : 스웨거 추가(클러스터)

--- src/main/java/com/monitoring/backend/dto/req/cluster/ClusterListReq.java
+++ src/main/java/com/monitoring/backend/dto/req/cluster/ClusterListReq.java
@@ -1,5 +1,6 @@
 package com.monitoring.backend.dto.req.cluster;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,12 +10,24 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "클러스터 목록 조회 요청")
 public class ClusterListReq {
 
 	//클러스터 타입으로 조회
+	@Schema(
+		description = "클러스터 타입정보 - 코드값",
+		example = "C0001",
+		nullable = true
+	)
 	private String clusterType;
 
+
 	//마지막으로 조회한 seq
+	@Schema(
+		description = "마지막으로 호출된 클러스터 seq - 무한스크롤 용",
+		example = "1",
+		nullable = true
+	)
 	private long lastSeq;
 
 
