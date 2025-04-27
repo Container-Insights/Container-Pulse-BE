2e530e1f5f6766894368015092abf786fb275a15
lsh9672 <lsh80165@gmail.com>
2025-04-27 22:11:17 +0900

    Feat : 스웨거 추가(컨테이너 그룹(pod))

--- src/main/java/com/monitoring/backend/dto/req/containergroup/ContainerGroupListReq.java
+++ src/main/java/com/monitoring/backend/dto/req/containergroup/ContainerGroupListReq.java
@@ -1,5 +1,6 @@
 package com.monitoring.backend.dto.req.containergroup;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,11 +10,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "컨테이너 그룹(pod) 목록 조회 요청")
 public class ContainerGroupListReq {
 
 	//노드 식별자에 해당하는 노드 조회용
+	@Schema(
+		description = "노드 식별자(시퀀스)",
+		example = "1"
+	)
 	private long nodeSeq;
 
 	//마지막으로 조회한 seq
+	@Schema(
+		description = "마지막으로 호출된 컨테이너 그룹 seq - 무한스크롤 용",
+		example = "1",
+		nullable = true
+	)
 	private long lastSeq;
 }
